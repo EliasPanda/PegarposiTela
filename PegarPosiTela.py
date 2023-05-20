3aba3e03ddae77d3b8d571b6fb1c664da025b789
Elias_Rodrigues <121892101+EliasPanda@users.noreply.github.com>
2023-05-20 19:39:25 -0300

    Create PegarPosiTela.py

---
+++ PegarPosiTela.py
@@ -0,0 +1,5 @@
+import pyautogui as bot 
+
+bot.sleep(5)#aqui você da o tempo antes de executar o programa
+print(bot.position()) #aqui executa o programa, você tem que deixar o mouse em cima para pegar a localização 
+#como está explicado no video.
